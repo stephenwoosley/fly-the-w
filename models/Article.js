4c0d2d58d4b9dd94f1938b0effdf89790bf74df6
Stephen Woosley <stephenrwoosley@gmail.com>
2017-09-22 16:37:46 -0400

    server.js for mongoose switchover, built index.html file

--- models/Article.js
+++ models/Article.js
@@ -15,7 +15,7 @@ var ArticleSchema = new Schema({
     type: String,
     required: true
   },
-  // This only saves one note's ObjectId, ref refers to the Note model
+  // saved boolean for saved/unsaved article
   saved: Boolean
 });
 
