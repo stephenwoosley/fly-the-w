950829098f5c5e7527099eeef779585fd1ab89ff
Stephen Woosley <stephenrwoosley@gmail.com>
2017-09-23 13:49:52 -0400

    Fixed app.js so articles populate

--- public/app.js
+++ public/app.js
@@ -7,3 +7,25 @@ $(".scrape").on("click", function(event) {
     console.log("maybe scraped?")
   })
 });
+
+$(".populate").on("click", function(){
+  $.ajax({
+    method: "GET",
+    url: "/articles"
+  }).done(function(data){
+    console.log("populate clicked!")
+    console.log(data);
+    for (article in data) {
+      $(".results").append(
+        "<li class='single-article'>"+
+          "Title: " + data[article].title +
+          "<hr>"+
+          "Link: " + data[article].link +
+          "<hr>"+
+        "</li>");
+    }
+
+  })
+})
+
+
